2354fb0f90b654a5442cc0b2dc010b2d6fb73f3e
Илья Смирнов <ilasmirnov39603@gmail.com>
2026-02-07 12:39:47 +0300

    Свойства состояния и фильтров MainViewModel

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -12,9 +12,23 @@ public partial class MainViewModel : ObservableObject
     // Коллекция транзакций для отображения в UI.
     public ObservableCollection<TransactionViewModel> Transactions { get; }
 
+    // Выбранная транзакция в списке (для редактирования и удаления).
+    [ObservableProperty]
+    private TransactionViewModel? _selectedTransaction;
+
+    // Начало периода для фильтрации.
+    [ObservableProperty]
+    private DateTime _startDateFilter;
+
+    // Конец периода для фильтрации.
+    [ObservableProperty]
+    private DateTime _endDateFilter;
+
     public MainViewModel(TransactionRepository repository)
     {
         _repository = repository;
         Transactions = new ObservableCollection<TransactionViewModel>();
+        _startDateFilter = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+        _endDateFilter = DateTime.Now.Date;
     }
 }
