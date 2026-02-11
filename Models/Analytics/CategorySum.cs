a702a775ccc26ec567ee35d74f7103a1239857b6
Илья Смирнов <ilasmirnov39603@gmail.com>
2026-02-12 00:11:26 +0300

    Добавлен пакет LiveCharts2 и модели аналитики (CategorySum, MonthSum, AnalyticsResult)

---
+++ Models/Analytics/CategorySum.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+
+namespace FinanceTracker.Models.Analytics;
+
+// Элемент сводки по категории (контракт JSON: name, sum).
+public class CategorySum
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("sum")]
+    public double Sum { get; set; }
+}
