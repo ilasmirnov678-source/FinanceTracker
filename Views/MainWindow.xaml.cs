aa3bda4e9dd6560fdd1d86b08679e16247d82499
Илья Смирнов <ilasmirnov39603@gmail.com>
2026-02-13 22:25:00 +0300

    Кастомная шапка окна: перетаскивание, двойной клик, восстановление в полный размер
    - Безрамочное окно (WindowStyle=None), своя шапка с кнопками ─/□/✕ и заголовком
    - Перетаскивание за шапку: при Maximized сначала восстановление, затем DragMove()
    - Двойной клик по шапке — развернуть/восстановить
    - При переходе из Maximized в Normal окно получает размер рабочей области (ApplyRestoredMaximizedBounds)

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using FinanceTracker.Services;
 using FinanceTracker.ViewModels;
 
@@ -22,6 +14,70 @@ namespace FinanceTracker.Views
             var repository = new TransactionRepository(dbService.ConnectionString);
             var pythonService = new PythonService();
             DataContext = new MainViewModel(repository, pythonService, dbService.DbPath);
+            StateChanged += (_, _) => UpdateMaximizeRestoreButtonContent();
+            Loaded += (_, _) => UpdateMaximizeRestoreButtonContent();
+        }
+
+        private void TitleBar_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left)
+                return;
+            if (e.ClickCount == 2)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    WindowState = WindowState.Normal;
+                    ApplyRestoredMaximizedBounds();
+                }
+                else
+                    WindowState = WindowState.Maximized;
+                UpdateMaximizeRestoreButtonContent();
+                return;
+            }
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                ApplyRestoredMaximizedBounds();
+                UpdateMaximizeRestoreButtonContent();
+            }
+            DragMove();
+        }
+
+        private void MinimizeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeRestoreButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                ApplyRestoredMaximizedBounds();
+            }
+            else
+                WindowState = WindowState.Maximized;
+            UpdateMaximizeRestoreButtonContent();
+        }
+
+        private void ApplyRestoredMaximizedBounds()
+        {
+            var workArea = SystemParameters.WorkArea;
+            Left = workArea.Left;
+            Top = workArea.Top;
+            Width = workArea.Width;
+            Height = workArea.Height;
+        }
+
+        private void UpdateMaximizeRestoreButtonContent()
+        {
+            MaximizeRestoreButton.Content = WindowState == WindowState.Maximized ? "❐" : "□";
+            MaximizeRestoreButton.ToolTip = WindowState == WindowState.Maximized ? "Восстановить" : "Развернуть";
+        }
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
